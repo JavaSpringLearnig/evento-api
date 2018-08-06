fce825d6f9e2c6791376f105f0fb4b1169b171e6
Weliton Martins <weliton.martinsprofissional@gmail.com>
2018-08-06 00:13:21 -0300

    configurando bean e hibernate

--- src/main/java/com/welitonmartins/models/Evento.java
+++ src/main/java/com/welitonmartins/models/Evento.java
@@ -1,6 +1,21 @@
 package com.welitonmartins.models;
 
-public class Evento {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class Evento implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Long codigo;
 	
 	private String nome;
 	private String local;
@@ -8,6 +23,13 @@ public class Evento {
 	private String horario;
 	
 	
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
